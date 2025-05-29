a337d94414d5de7f7311473a1835c0963c3622a3
Sammie156 <samanwayadatta@gmail.com>
2025-05-29 15:33:47 +0530

    Change color of done task to green.

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ let tasks_markup = [];
 const task_listElement = document.querySelector(".task-container");
 
 const text_input = document.querySelector("#add-task");
-
 text_input.addEventListener("keypress", (e) => {
   if (e.key == "Enter") addTask();
 });
@@ -36,21 +35,6 @@ function renderTasksMarkup() {
   `;
 
   task_listElement.innerHTML = markup;
-
-  // Commented for future reference
-  // const all_tasks = document.querySelectorAll("label");
-
-  // for (const task of all_tasks) {
-  //   task.addEventListener("click", function () {
-  //     const found = tasks.find((t) => t.id == task.id);
-  //     if (found) {
-  //       console.log(task);
-  //       task.classList.toggle("done");
-  //       console.log(task);
-  //       found.status = !found.status;
-  //     }
-  //   });
-  //
 }
 
 function taskComplete(id) {
